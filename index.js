e4a86161309831a9e38c15bbb07cc1e13a9bc25f
joaopedromsantos <joaomartinscrf2019@gmail.com>
2023-12-18 20:29:13 -0300

    teste de teste

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ fastify.get('/', async (request, reply) => {
   return { message: 'Hello World!' };
 });
 
+fastify.setErrorHandler((error, request, reply) => {
+  console.error(error);
+  reply.status(500).send({ error: 'Internal Server Error' });
+});
+
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT || 3000 });
+    await fastify.listen(process.env.PORT, '0.0.0.0');
     console.log(`Server started successfully on port ${process.env.PORT}!`);
   } catch (err) {
     console.error(err);
@@ -15,4 +20,3 @@ const start = async () => {
 };
 
 start();
-
